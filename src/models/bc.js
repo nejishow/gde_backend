e4f08fe4a9ad51fe3c039ceb300bf283f26fe337
Ashraf Mohamed <41853901+nejishow@users.noreply.github.com>
2020-08-31 17:56:18 +0300

    update db

--- src/models/bc.js
+++ src/models/bc.js
@@ -1,29 +1,67 @@
-const mongoose = require('mongoose')
-const boiteSchema = new mongoose.Schema({
-    number: {
-        type: String,
-        //required: [true, 'Why no values? Always provide values!'],
+const mongoose = require("mongoose");
+const b_commandesSchema = new mongoose.Schema(
+  {
+    Num_cmd: {
+      type: String,
+      //required: [true, 'Why no values? Always provide values!'],
     },
-    boiteType: {
-        type: String,
-        //required: [true, 'Why no values? Always provide values!'],
+    Date_cmd: {
+      type: String,
+      //required: [true, 'Why no values? Always provide values!'],
     },
-    idBoiteType: {
-        type: mongoose.Schema.Types.ObjectId,
-        //required: true
+    Code_service: {
+      type: String,
+      //required: [true, 'Why no values? Always provide values!'],
     },
-    price: {
-        type: Number,
-        //required: [true, 'Why no values? Always provide values!'],
+    idService: {
+      type: mongoose.Schema.Types.ObjectId,
+      //required: true
+    },
+    Code_fournisseur: {
+      type: String,
+      //required: [true, 'Why no values? Always provide values!'],
+    },
+    idFournisseur: {
+      type: mongoose.Schema.Types.ObjectId,
+      //required: true
+    },
+    Devises: {
+      type: String,
+    },
+    Taux: {
+      type: String,
+    },
+    Objet_cmd: {
+      type: String,
+    },
+    Num_compte: {
+      type: String,
+    },
+    idCompte: {
+      type: mongoose.Schema.Types.ObjectId,
+      //required: true
+    },
+    Exercice: {
+      type: String,
+    },
+    Montant: {
+      type: String,
+      //required: [true, 'Why no values? Always provide values!'],
+    },
+    PJ: {
+      type: String,
+    },
+    Etat: {
+      type: String,
     },
     enabled: {
-        type: Boolean,
-        default: true
-    }
-},
-{timestamps: true})
+      type: Boolean,
+      default: true,
+    },
+  },
+  { timestamps: true }
+);
 
+const BONS = mongoose.model("bon_commandes", b_commandesSchema);
 
-const Boite = mongoose.model('boites', boiteSchema)
-
-module.exports = Boite
+module.exports = BONS;
---
+++ src/routers/bc.js
@@ -0,0 +1,43 @@
+const express = require("express")
+const router = new express.Router()
+// const auth = require('../middleware/auth')
+const BC = require("../models/bc")
+
+
+router.post('/bon', async (req, res) => {
+    const bon = await new BC(req.body)
+    try {
+        bon.save()
+        return res.status(201).send(bon)
+    } catch (error) {
+        res.status(400).send(error)
+    }
+})
+
+router.get('/bon/:id', async (req, res) => {
+    try {
+        const bon = await BC.findById({_id:req.params.id})
+        if (!bon) {
+            return res.status(200).send({message: "Aucun bon trouvé"})
+
+        }
+        return res.status(201).send(bon)
+    } catch (error) {
+        res.status(400).send(error)
+    }
+})
+
+router.get('/bons/:date', async (req, res) => {
+    try {
+        console.log(req.params.date);
+        const bons = await BC.find({Exercice:req.params.date})
+        if (!bons) {
+            return res.status(200).send([])
+        }
+        return res.status(201).send(bons)
+    } catch (error) {
+        res.status(400).send(error)
+    }
+})
+
+module.exports = router
