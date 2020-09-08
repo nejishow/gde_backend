4921b4f9fc837e74b084e33532304974d67c0322
Ashraf Mohamed <41853901+nejishow@users.noreply.github.com>
2020-09-08 12:53:19 +0300

    mandat en cours

--- src/models/mandat.js
+++ src/models/mandat.js
@@ -1,51 +1,51 @@
-const mongoose = require('mongoose')
-const mandatSchema = new mongoose.Schema({
-        Num_cmd: {
-          type: String,
-          //required: [true, 'Why no values? Always provide values!'],
-        },
-        Num_Mandat: {
-            type: String,
-            //required: [true, 'Why no values? Always provide values!'],
-          },
-        Date_Mandat: {
-          type: String,
-          //required: [true, 'Why no values? Always provide values!'],
-        },
-        Objet_depense: {
-          type: String,
-        },
-        Num_facture: {
-          type: String,
-        },
-        Num_cheque: {
-            type: String,
-          },
-          Num_CB: {
-            type: String,
-          },
-        Montant_mandat: {
-          type: String,
-          //required: [true, 'Why no values? Always provide values!'],
-        },
-        PJ: {
-          type: String,
-        },
-        Code_Paiement: {
-          type: String,
-        },
-        enabled: {
-          type: Boolean,
-          default: true,
-        },
-        Paye: {
-            type: Boolean,
-            default: true,
-          },
-      },
-      { timestamps: true }
-    );
+const mongoose = require("mongoose");
+const mandatSchema = new mongoose.Schema(
+  {
+    Num_cmd: {
+      type: String,
+      //required: [true, 'Why no values? Always provide values!'],
+    },
+    Num_Mandat: {
+      type: String,
+      //required: [true, 'Why no values? Always provide values!'],
+    },
+    Date_Mandat: {
+      type: String,
+      //required: [true, 'Why no values? Always provide values!'],
+    },
+    Objet_depense: {
+      type: String,
+    },
+    Num_facture: {
+      type: String,
+    },
+    Num_cheque: {
+      type: String,
+    },
+    Num_CB: {
+      type: String,
+    },
+    Montant_mandat: {
+      type: String,
+      //required: [true, 'Why no values? Always provide values!'],
+    },
+    PJ: {
+      type: String,
+    },
+    Code_Paiement: {
+      type: String,
+    },
+    enabled: {
+      type: Boolean,
+      default: true,
+    },
+    Paye: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
 
-const MANDAT = mongoose.model('mandats', mandatSchema)
+const MANDAT = mongoose.model("mandats", mandatSchema);
 
-module.exports = MANDAT
+module.exports = MANDAT;
